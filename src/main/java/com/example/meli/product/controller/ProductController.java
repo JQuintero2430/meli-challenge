54760c63bf43e43d7587d9112993b99d88169344
jorge.quintero <jfqs24@gmail.com>
2025-07-17 13:34:02 -0500

    Initial commit

--- src/main/java/com/example/meli/product/controller/ProductController.java
+++ src/main/java/com/example/meli/product/controller/ProductController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -15,8 +16,8 @@ public class ProductController {
 
   private final ProductService productService;
 
-  @GetMapping("{slug}/{id}")
-  public ProductDto getProductBySlugAndId(@PathVariable String slug, @PathVariable Long id) {
-    return productService.getProductBySlugAndId(slug, id);
+  @GetMapping("{slug}")
+  public ProductDto getProductBySlugAndId(@PathVariable String slug, @RequestParam("productId") Long productId) {
+    return productService.getProductBySlugAndId(slug, productId);
   }
 }
