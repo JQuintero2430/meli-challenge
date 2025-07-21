1b05d0c10c748fd20266942060a1376620cd611e
jorge.quintero <jfqs24@gmail.com>
2025-07-21 09:35:34 -0500

    new iteration

--- meli-fe/astro.config.mjs
+++ meli-fe/astro.config.mjs
@@ -1,15 +1,9 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-import tailwindcss from '@tailwindcss/vite';
-
-// https://astro.build/config
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 4321
-  },
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  output: 'server',
+  adapter: node({
+    mode: 'standalone'
+  }),
 });
