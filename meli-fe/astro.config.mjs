41d6489544147c5fc9386eac554eaa2a4fcafb8e
jorge.quintero <jfqs24@gmail.com>
2025-07-21 19:30:55 -0500

    new iteration

--- meli-fe/astro.config.mjs
+++ meli-fe/astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 
 export default defineConfig({
-  output: 'hybrid',
+  output: 'server',
   adapter: node({
     mode: 'standalone'
   }),
+  server: {
+    host: '0.0.0.0',
+    port: 4321
+  }
 });
